ba6aa324bd72b3df9f2ee8ffdaa0050943cbcae9
Pier-Luc Gendreau <pierluc@outlook.com>
2020-09-12 22:16:50 -0400

    Remove Storybook docs addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,14 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.@(ts|tsx)'],
-  addons: ['@storybook/addon-docs'],
-  webpackFinal: async config => {
+  stories: ['../stories/**/*.stories.tsx'],
+  webpackFinal: config => {
     config.module.rules.push({
-      test: /\.(ts|tsx)$/,
+      test: /\.tsx$/,
       use: [
         {
           loader: require.resolve('ts-loader'),
           options: {
             transpileOnly: true,
           },
-        },
-        {
-          loader: require.resolve('react-docgen-typescript-loader'),
         },
       ],
     });
