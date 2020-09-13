367bb4a0753fd6c3609e4eab0c3054ef35edc873
Pier-Luc Gendreau <pierluc@outlook.com>
2020-09-13 15:47:05 -0400

    Add some tests

--- test/Tabs.test.tsx
+++ test/Tabs.test.tsx
@@ -1,57 +1,71 @@
+import '@testing-library/jest-dom/extend-expect';
+import { fireEvent, render } from '@testing-library/react';
 import React from 'react';
-import * as ReactDOM from 'react-dom';
 import * as Tabs from '../stories/Tabs.stories';
 
 describe('Basic', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
+    const { getAllByText, getByText } = render(<Tabs.Basic />);
 
-    ReactDOM.render(<Tabs.Basic />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    expect(getAllByText('My Account')).toHaveLength(2);
+    expect(getByText('Company')).toBeTruthy();
+    expect(getByText('Team Members')).toBeTruthy();
+
+    const billing = getByText('Billing');
+
+    fireEvent.click(billing);
+
+    expect(getAllByText('Billing')).toHaveLength(2);
   });
 });
 
 describe('Browser', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
+    const { getAllByText, getAllByTitle, getByText, getByTitle } = render(
+      <Tabs.Browser />
+    );
 
-    ReactDOM.render(<Tabs.Browser />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    expect(getAllByText(/^Tab (1|2|3)$/)).toHaveLength(4);
+    expect(getAllByTitle('Close tab')).toHaveLength(3);
+
+    const [closeTab] = getAllByTitle('Close tab');
+
+    fireEvent.click(closeTab);
+
+    expect(getAllByText(/^Tab (2|3)$/)).toHaveLength(3);
+
+    const newTab = getByTitle('New tab');
+
+    fireEvent.click(newTab);
+
+    expect(getAllByText(/^Tab (2|3|4)$/)).toHaveLength(4);
+
+    fireEvent.click(getByText('Tab 4'));
+
+    expect(getAllByText('Tab 4')).toHaveLength(2);
   });
 });
 
 describe('Draggable', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-
-    ReactDOM.render(<Tabs.Draggable />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    render(<Tabs.Draggable />);
   });
 });
 
 describe('Dropdown', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-
-    ReactDOM.render(<Tabs.Dropdown />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    render(<Tabs.Dropdown />);
   });
 });
 
 describe('Nested', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-
-    ReactDOM.render(<Tabs.Nested />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    render(<Tabs.Nested />);
   });
 });
 
 describe('Overflow', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-
-    ReactDOM.render(<Tabs.Overflow />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    render(<Tabs.Overflow />);
   });
 });
