c45492f3a036b46c54a429240b6c46d70f6ac9c9
Pier-Luc Gendreau <plgendreau@focusoptimization.com>
2021-06-08 09:47:50 -0400

    Fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,3 @@
 module.exports = {
   stories: ['../stories/**/*.stories.tsx'],
-  webpackFinal: config => {
-    config.module.rules.push({
-      test: /\.tsx$/,
-      use: [
-        {
-          loader: require.resolve('ts-loader'),
-          options: {
-            transpileOnly: true,
-          },
-        },
-      ],
-    });
-
-    config.resolve.extensions.push('.tsx');
-
-    return config;
-  },
 };
