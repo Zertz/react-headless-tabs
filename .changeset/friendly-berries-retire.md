cf360ef61b921ca08f3dabfa47a2772253a2d6ab
Pier-Luc Gendreau <Zertz@users.noreply.github.com>
2022-05-15 08:53:30 -0400

    Add different unmounting strategies (#377)
    * Add different unmounting strategies
    
    * Create friendly-berries-retire.md
    
    * Improve code and docs
    
    * Update Documentation.tsx

---
+++ .changeset/friendly-berries-retire.md
@@ -0,0 +1,6 @@
+---
+"docs": minor
+"react-headless-tabs": minor
+---
+
+Add different unmounting strategies
--- packages/lib/src/TabPanel.tsx
+++ packages/lib/src/TabPanel.tsx
@@ -3,28 +3,39 @@ import * as React from "react";
 export function TabPanel({
   children,
   render = "idle",
+  unmount = "never",
   ...props
 }: React.HTMLAttributes<HTMLDivElement> & {
   hidden: boolean;
-  render?: "always" | "idle" | "lazy";
-}) {
+} & (
+    | {
+        render?: "always" | "idle";
+        unmount?: "never";
+      }
+    | {
+        render?: "lazy";
+        unmount?: "always" | "idle" | "never";
+      }
+  )) {
   const [shouldRender, setShouldRender] = React.useState(
     !props.hidden || render === "always"
   );
 
   React.useEffect(() => {
-    if (props.hidden) {
-      if (render === "idle") {
-        ("requestIdleCallback" in window ? requestIdleCallback : setTimeout)(
-          () => setShouldRender(true)
-        );
-      }
-
-      return;
+    if (!props.hidden || render === "always") {
+      setShouldRender(true);
+    } else if (render === "idle") {
+      ("requestIdleCallback" in window ? requestIdleCallback : setTimeout)(() =>
+        setShouldRender(true)
+      );
+    } else if (unmount === "always") {
+      setShouldRender(false);
+    } else if (unmount === "idle") {
+      ("requestIdleCallback" in window ? requestIdleCallback : setTimeout)(() =>
+        setShouldRender(false)
+      );
     }
-
-    setShouldRender(true);
-  }, [props.hidden, render]);
+  }, [props.hidden, render, unmount]);
 
   return <div {...props}>{shouldRender ? children : null}</div>;
 }
