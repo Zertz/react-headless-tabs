5615602365c4a286d4dd860723c1ed6dc4adfea5
Pier-Luc Gendreau <pierluc@outlook.com>
2022-10-09 14:44:13 -0400

    Hello

--- packages/lib/test/TabPanel.test.tsx
+++ packages/lib/test/TabPanel.test.tsx
@@ -4,58 +4,57 @@ import { expect, test } from "vitest";
 import { TabPanel } from "../src/TabPanel";
 
 test("default", () => {
-  const { getByText } = render(<TabPanel hidden={false}>TabPanel</TabPanel>);
+  const { getByText } = render(<TabPanel hidden={false}>Hello</TabPanel>);
 
-  expect(getByText("TabPanel")).toBeTruthy();
+  expect(getByText("Hello")).toBeTruthy();
 });
 
 test("valid: render/always", () => {
   const { getByText } = render(
     <TabPanel hidden={false} render="always">
-      TabPanel
+      Hello
     </TabPanel>
   );
 
-  expect(getByText("TabPanel")).toBeTruthy();
+  expect(getByText("Hello")).toBeTruthy();
 });
 
 test("invalid: render/always + unmount/never", () => {
   const { getByText } = render(
-    // @ts-expect-error render="always" cannot be used with unmount
     <TabPanel hidden={false} render="always" unmount="never">
-      TabPanel
+      Hello
     </TabPanel>
   );
 
-  expect(getByText("TabPanel")).toBeTruthy();
+  expect(getByText("Hello")).toBeTruthy();
 });
 
 test("valid: render/default + unmount/number", () => {
   const { getByText } = render(
     <TabPanel hidden={false} unmount={60}>
-      TabPanel
+      Hello
     </TabPanel>
   );
 
-  expect(getByText("TabPanel")).toBeTruthy();
+  expect(getByText("Hello")).toBeTruthy();
 });
 
 test("valid: render/idle + unmount/number", () => {
   const { getByText } = render(
     <TabPanel hidden={false} render="idle" unmount={60}>
-      TabPanel
+      Hello
     </TabPanel>
   );
 
-  expect(getByText("TabPanel")).toBeTruthy();
+  expect(getByText("Hello")).toBeTruthy();
 });
 
 test("valid: render/lazy + unmount/number", () => {
   const { getByText } = render(
     <TabPanel hidden={false} render="lazy" unmount={60}>
-      TabPanel
+      Hello
     </TabPanel>
   );
 
-  expect(getByText("TabPanel")).toBeTruthy();
+  expect(getByText("Hello")).toBeTruthy();
 });
