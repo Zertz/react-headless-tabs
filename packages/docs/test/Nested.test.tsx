ac4aad9acf72067c2b1ec93e6b9470a26174a0de
Pier-Luc Gendreau <pierluc@outlook.com>
2022-10-09 15:39:52 -0400

    Revert "tests"
    This reverts commit 88530a6d172f7c15d3e85e5f0afab9ee7d5bbdcd.

--- packages/docs/test/Nested.test.tsx
+++ packages/docs/test/Nested.test.tsx
@@ -1,16 +1,13 @@
 import { render } from "@testing-library/react";
-import { act } from "react-dom/test-utils";
 import { expect, test } from "vitest";
 import { Nested } from "../examples/Nested";
 
 test("Nested", () => {
   const { getAllByText, getByText } = render(<Nested />);
 
+  expect(getAllByText("My Account")).toHaveLength(2);
   expect(getByText("Company")).toBeTruthy();
-  expect(getByText("Settings")).toBeTruthy();
 
-  act(() => {
-    expect(getAllByText("My Account")).toHaveLength(2);
-    expect(getAllByText("Profile")).toHaveLength(2);
-  });
+  expect(getAllByText("Profile")).toHaveLength(2);
+  expect(getByText("Settings")).toBeTruthy();
 });
