88530a6d172f7c15d3e85e5f0afab9ee7d5bbdcd
Pier-Luc Gendreau <pierluc@outlook.com>
2022-10-09 15:36:27 -0400

    tests

--- packages/docs/test/Basic.test.tsx
+++ packages/docs/test/Basic.test.tsx
@@ -1,12 +1,16 @@
 import { render } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import { expect, test } from "vitest";
 import { Basic } from "../examples/Basic";
 
 test("Basic", () => {
   const { getAllByText, getByText } = render(<Basic />);
 
-  expect(getAllByText("My Account")).toHaveLength(2);
   expect(getByText("Company")).toBeTruthy();
   expect(getByText("Team Members")).toBeTruthy();
   expect(getByText("Billing")).toBeTruthy();
+
+  act(() => {
+    expect(getAllByText("My Account")).toHaveLength(2);
+  });
 });
--- packages/docs/test/Nested.test.tsx
+++ packages/docs/test/Nested.test.tsx
@@ -1,13 +1,16 @@
 import { render } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import { expect, test } from "vitest";
 import { Nested } from "../examples/Nested";
 
 test("Nested", () => {
   const { getAllByText, getByText } = render(<Nested />);
 
-  expect(getAllByText("My Account")).toHaveLength(2);
   expect(getByText("Company")).toBeTruthy();
-
-  expect(getAllByText("Profile")).toHaveLength(2);
   expect(getByText("Settings")).toBeTruthy();
+
+  act(() => {
+    expect(getAllByText("My Account")).toHaveLength(2);
+    expect(getAllByText("Profile")).toHaveLength(2);
+  });
 });
