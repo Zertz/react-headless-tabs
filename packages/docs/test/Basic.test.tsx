2223e1ace28f5faa759aa4c803b597e0c1fa432b
Pier-Luc Gendreau <pierluc@outlook.com>
2022-10-09 22:15:16 -0400

    Update Basic.test.tsx

--- packages/docs/test/Basic.test.tsx
+++ packages/docs/test/Basic.test.tsx
@@ -5,8 +5,12 @@ import { Basic } from "../examples/Basic";
 test("Basic", async () => {
   const { getAllByText, getByText } = render(<Basic />);
 
-  await waitFor(() => expect(getAllByText("My Account")).toHaveLength(2));
+  expect(getAllByText("My Account")).toHaveLength(2);
   expect(getByText("Company")).toBeTruthy();
   expect(getByText("Team Members")).toBeTruthy();
   expect(getByText("Billing")).toBeTruthy();
+
+  await waitFor(() => expect(getAllByText("Company")).toHaveLength(2));
+  expect(getAllByText("Team Members")).toHaveLength(2);
+  expect(getAllByText("Billing")).toHaveLength(2);
 });
